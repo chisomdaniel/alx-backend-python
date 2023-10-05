f6ae43b7df496fca7b9a844ff4e02db2e465d2b7
Chisom Daniel <chinwezechisom@gmail.com>
2023-10-05 23:51:47 +0100

    almostdone

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Complex types - string and int/float to tuple'''
-from typing import List, Union, Tuple
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[int, float]:
