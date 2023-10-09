d3a8fbe5f786b9557a5ea9deec3a7efe9c10dbb3
Chisom Daniel <chinwezechisom@gmail.com>
2023-10-09 09:33:44 +0100

    advanced

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 '''Duck typing - first element of a sequence'''
-from typing import Any, Union, Mapping, Optional
+from typing import Any, Union, Mapping, Optional, TypeVar
+
+T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[Optional[Any], None] = None) -> Union[Any, Optional[Any]]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     '''Given the parameters and the return values, add
     type annotations to the function'''
     if key in dct:
         return dct[key]
     else:
         return default
-
-
-
-annotations = safely_get_value.__annotations__
-
-print("Here's what the mappings should look like")
-for k, v in annotations.items():
-    print( ("{}: {}".format(k, v)))
