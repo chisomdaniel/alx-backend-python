03c3e6d9bf9d3319ff332d22dd897624269d1e48
Chisom Daniel <chinwezechisom@gmail.com>
2023-10-30 21:54:42 +0100

    more-task

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''test client test file'''
+from client import GithubOrgClient
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch
+from unittest.mock import Mock
+from utils import get_json
+
+
+class TestGithubOrgClient(unittest.TestCase):
+
+    @parameterized.expand([
+        ("google", {"key", "value"}),
+        ("abc", {"key", "value"})
+    ])
+    @patch("utils.get_json")
+    def test_org(self, org_name, expected, mock_func):
+        mock_func.return_value = expected
+        url = "https://api.github.com/orgs/{}".format(org_name)
+        GithubOrgClient(org_name).org
+
+        #mock_func.assert_called_once_with(url)
